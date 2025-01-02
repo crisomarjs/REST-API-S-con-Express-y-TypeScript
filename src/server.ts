9cdafe2b91452fa8ab3fca8023f16fc290257215
crisomarjs <cristianomar71@outlook.com>
2025-01-01 22:09:08 -0600

    Se documenta API con Swagger

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from "express";
 import router from "./router";
 import db from "./config/db";
 import colors from "colors";
+import swaggerUi from "swagger-ui-express";
+import swaggerSpec from "./config/swagger";
 
 
 // Connect to the database
@@ -25,9 +27,10 @@ server.use(express.json());
 
 server.use('/api/products' , router);
 
-server.get('/api', (req, res) => {
-    res.json({msg: 'Desde API'});
-});
+//Docs
+server.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+
 
 
 export default server;
